16b214d7c9504038a740cbecde6634a681c64f29
Andres Quinones <anguquga@gmail.com>
2020-09-18 16:19:37 -0500

    Module 5 - React Router Added and Routing base setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import Homepage from "./containers/Homepage/Homepage";
@@ -6,7 +8,9 @@ import Homepage from "./containers/Homepage/Homepage";
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
